3860a790bcd56d9b040902f7c88cee4430efacce
jesusm2000 <ivanmoreiraloor2019@hotmail.com>
2024-07-29 23:15:53 -0500

    Cambios en el navbar y dashboard

--- index.php
+++ index.php
@@ -62,8 +62,11 @@ $selected_dashboard = isset($_POST['dashboard']) ? $_POST['dashboard'] : null;
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0 user-scalable=no">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <title>Menu Dashboard</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <!-- BOX ICONS -->
     <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
     <!-- BOOTSTRAP ICONS -->
@@ -77,7 +80,7 @@ $selected_dashboard = isset($_POST['dashboard']) ? $_POST['dashboard'] : null;
             position: absolute;
             bottom: 20px;
             left: 20px;
-            background-color: rgba(44, 62, 80, 0.8);
+            background-color: #423187;
             color: white;
             padding: 10px;
             border-radius: 5px;
@@ -142,6 +145,19 @@ $selected_dashboard = isset($_POST['dashboard']) ? $_POST['dashboard'] : null;
             font-size: 18px;
             color: black;
         }
+        .navbar {
+            width: 100%; /* Asegura que la navbar ocupe todo el ancho */
+            position: static; /* Mantiene la navbar en la parte superior */
+            top: 0; /* Fija la navbar en la parte superior */
+            z-index: 1000; /* Asegura que la navbar esté por encima de otros elementos */
+            background-color: #423187; /* Color de fondo de la navbar */
+        }
+        .tablero {
+            margin-top: 0px; /* Ajusta este valor a la altura de tu navbar */
+            flex: 1; /* Permite que el tablero ocupe el espacio restante */
+            padding: 20px; /* Añade algo de padding para mayor separación */
+            overflow: auto; /* Permite el desplazamiento si el contenido es más grande */
+        }
     </style>
 </head>
 <body>
@@ -169,7 +185,14 @@ $selected_dashboard = isset($_POST['dashboard']) ? $_POST['dashboard'] : null;
         </div>
     </div>
     <div class="seccion">
-        <div id="message-container" class="message-container">
+        <div>
+        <nav class="navbar navbar-expand-lg" style="background-color:#423187;">
+            <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse " id="navbarSupportedContent" >
+            <div>
             <?php
             if ($current_role == 'Director') {
                 echo '<p>Bienvenido Director.</p>';
@@ -195,21 +218,24 @@ $selected_dashboard = isset($_POST['dashboard']) ? $_POST['dashboard'] : null;
             }
             ?>
         </div>
+            </div>
+        </div>
+        </nav>
+        </div>
         <?php if ($selected_dashboard || $current_role == 'Docente'): ?>
             <div class="tablero">
                 <?php if ($selected_dashboard == 'TAREAS' && $current_role != 'Docente'): ?>
-                    <iframe title="Dashboard_Vinculacion - Tareas" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=2f567d7d-83fe-4285-a804-87af34c1c389&autoAuth=true&ctid=d9a7c315-62a6-4cb6-b905-be798b1d5076&navContentPaneEnabled=false" frameborder="0" allowFullScreen="true"></iframe>
+                    <iframe title="Dashboard_Vinculacion - Tareas" width="1250" height="600" src="https://app.powerbi.com/reportEmbed?reportId=2f567d7d-83fe-4285-a804-87af34c1c389&autoAuth=true&ctid=d9a7c315-62a6-4cb6-b905-be798b1d5076&navContentPaneEnabled=false" frameborder="0" allowFullScreen="true"></iframe>
                 <?php elseif ($selected_dashboard == 'PROYECTOS'): ?>
-                    <iframe title="Dashboard_Vinculacion - Proyectos" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=2f567d7d-83fe-4285-a804-87af34c1c389&autoAuth=true&ctid=d9a7c315-62a6-4cb6-b905-be798b1d5076&navContentPaneEnabled=false&pageName=d3a902f0a34f1c82b329" frameborder="0" allowFullScreen="true"></iframe>
+                    <iframe title="Dashboard_Vinculacion - Proyectos" width="1250" height="600" src="https://app.powerbi.com/reportEmbed?reportId=2f567d7d-83fe-4285-a804-87af34c1c389&autoAuth=true&ctid=d9a7c315-62a6-4cb6-b905-be798b1d5076&navContentPaneEnabled=false&pageName=d3a902f0a34f1c82b329" frameborder="0" allowFullScreen="true"></iframe>
                 <?php elseif ($selected_dashboard == 'INSTITUCIONES' || $current_role == 'Docente'): ?>
-                    <iframe title="Dashboard_Vinculacion - Instituciones" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=2f567d7d-83fe-4285-a804-87af34c1c389&autoAuth=true&ctid=d9a7c315-62a6-4cb6-b905-be798b1d5076&navContentPaneEnabled=false&pageName=192b6339f0de780f4904" frameborder="0" allowFullScreen="true"></iframe>
+                    <iframe title="Dashboard_Vinculacion - Instituciones" width="1250" height="600" src="https://app.powerbi.com/reportEmbed?reportId=2f567d7d-83fe-4285-a804-87af34c1c389&autoAuth=true&ctid=d9a7c315-62a6-4cb6-b905-be798b1d5076&navContentPaneEnabled=false&pageName=192b6339f0de780f4904" frameborder="0" allowFullScreen="true"></iframe>
                 <?php endif; ?>
             </div>
         <?php endif; ?>
     </div>
     <div class="user-info">
         <p><i class="bi bi-person-circle"></i>Usuario: <?php echo $_SESSION['usuario']; ?></p>
-        <br>
         <p><i class="bi bi-gear"></i>Rol: <span id="current-role"><?php echo $current_role; ?></span></p>
     </div>
     <form method="post" style="display: none;" id="role-form">
@@ -240,5 +266,6 @@ $selected_dashboard = isset($_POST['dashboard']) ? $_POST['dashboard'] : null;
             }
         });
     </script>
+    
 </body>
 </html>
